048d260817329b94e74db7cb801bb5676038333c
Suhrob <suhrob.a001@gmail.com>
2025-04-10 15:22:16 +0500

    Ready to use, python telegram bot template aiogram==2.14

---
+++ handlers/users/start.py
@@ -0,0 +1,18 @@
+import asyncpg
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandStart
+
+from loader import dp, db, bot
+from data.config import ADMINS
+
+
+@dp.message_handler(CommandStart())
+async def bot_start(message: types.Message):
+    try:
+        user = await db.add_user(telegram_id=message.from_user.id,
+                                 full_name=message.from_user.full_name,
+                                 username=message.from_user.username)
+    except asyncpg.exceptions.UniqueViolationError:
+        user = await db.select_user(telegram_id=message.from_user.id)
+
+    await message.answer(f"Salom, {message.from_user.full_name}! Botimizga xush kelibsiz")
