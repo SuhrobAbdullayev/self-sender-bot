428cd02268c329ade8d931ffb88a82f91d6bcaa8
Suhrob <suhrob.a001@gmail.com>
2025-04-11 05:22:21 +0500

    updated

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -20,15 +20,18 @@ async def handle_ads(message: types.Message):
     if channel_data is None:
         return
     if message.media_group_id:
-        await bot.send_message(chat_id=message.chat.id, text="❌ It is impossible to send collections of media (multiple photos) at once.")
+        await bot.send_message(chat_id=message.chat.id, text="❌ Kop rasmli reklamalarni yuborish imkonsiz. Iltimos bunday reklamalarni admin orqali yuboring. \n\n Admin: @Ish_Admin_AS")
         return
     if channel_data:
         channel_id = channel_data['channel_id']
         count = channel_data['counts'] - 1
-        if count <= 0:
+        if channel_data['counts'] <= 0:
             await bot.send_message(chat_id=message.chat.id, text=f"⭕️ Sizda yuborish uchun reklama qolmadi. \n\n- Reklama sotib olish uchun @Ish_Admin_AS adminga yozing.")
             return
-        await db.update_count(chat_id = str(message.chat.id), count = count)
-        await bot.copy_message(from_chat_id=message.chat.id, chat_id=channel_id, message_id=message.message_id)
-        await bot.copy_message(from_chat_id=message.chat.id, chat_id=5606952183, message_id=message.message_id)
-        await bot.send_message(chat_id=message.chat.id, text=f"Reklamangiz yuborildi ✅ \n\n- Sizda {count} ta reklama qoldi")
+        try:
+            await db.update_count(chat_id = str(message.chat.id), count = count)
+            await bot.copy_message(from_chat_id=message.chat.id, chat_id=channel_id, message_id=message.message_id)
+            await bot.copy_message(from_chat_id=message.chat.id, chat_id=5606952183, message_id=message.message_id)
+            await bot.send_message(chat_id=message.chat.id, text=f"Reklamangiz yuborildi ✅ \n\n- Sizda {count} ta reklama qoldi")
+        except Exception as e:
+            await bot.send_message(chat_id=5606952183, text=e)
